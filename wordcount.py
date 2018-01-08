7f382f5a766df29cc96c1a36d6e9b15d250a6420
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 12:39:55 -0800

    used collections.Counter class

--- wordcount.py
+++ wordcount.py
@@ -1,19 +1,30 @@
 import sys
+from collections import Counter
 
 text_file = open(sys.argv[1])
-
-word_count = {}
-
-for line in text_file:
-    line = line.rstrip()
-    line = line.lower()
-    for c in line:
-        if c in "?.!/;:,'\"_":
-            line = line.replace(c, "")
-    words = line.split()
-    for word in words:
-        word_count[word] = word_count.get(word, 0) + 1
-
-
+text_file = text_file.read()
+text_file = text_file.lower()
+for c in text_file:
+    if c in "?.!/;:,'\"_":
+        text_file = text_file.replace(c, "")
+words = text_file.split()
+word_count = Counter(words)
 for word, count in word_count.items():
     print word, count
+
+
+# word_count = {}
+
+# for line in text_file:
+#     line = line.rstrip()
+#     line = line.lower()
+#     for c in line:
+#         if c in "?.!/;:,'\"_":
+#             line = line.replace(c, "")
+#     words = line.split()
+#     for word in words:
+#         word_count[word] = word_count.get(word, 0) + 1
+
+
+# for word, count in word_count.items():
+#     print word, count
